c4b3f4c28ba10e0e09369445fe32389322c9eac9
Muskaa44 <u2022468@giki.edu.pk>
2025-03-19 10:12:26 +0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,9 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(15))  # Optional field
+    dob= db.column(db.date,nulllable=true)
+    
